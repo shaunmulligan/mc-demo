c39ccc87685ad23821fa1f355829814b1746a191
Shaun Mulligan <switchflip.shaun@gmail.com>
2018-02-15 12:21:09 +0000

    ...

--- data1/index.js
+++ data1/index.js
@@ -1,8 +1,5 @@
 var http = require('http');
-var io = require('socket.io');
-var express = require('express');
-
-var PORT = 8080;
+var fs = require('fs');
 
 function random (low, high) {
 	return Math.random() * (high - low) + low;
@@ -21,16 +18,21 @@ function randomEmit (socket, x) {
   }, 1000);
 }
 
-var app = express();
-var server = http.createServer(app);
-
-app.use(express.static(__dirname + '/static'))
-server.listen(PORT, function() {
-	console.log("server is listening on port", PORT);
+// Loading the index file . html displayed to the client
+var server = http.createServer(function(req, res) {
+    fs.readFile('./index.html', 'utf-8', function(error, content) {
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(content);
+    });
 });
 
-io = io.listen(app);
+// Loading socket.io
+var io = require('socket.io').listen(server);
 
-io.on('connection', function(socket) {
-	randomEmit(socket, 0);
+// When a client connects, we note it in the console
+io.sockets.on('connection', function (socket) {
+    console.log('A client is connected!');
+    randomEmit(socket, 0);
 });
+
+server.listen(8080);
